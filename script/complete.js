8b448bd1e7f486762337042aee8e28fd7c2d17d3
Arin Barua <arinbarua1994@gmail.com>
2025-03-03 19:07:03 +0600

    added final design

---
+++ script/complete.js
@@ -0,0 +1,16 @@
+
+//   alert("Hello! I am an alert box!");
+
+function getColor() {
+    let a = Math.floor(Math.random() * 360);
+    let b = Math.floor(Math.random() * 50) + 50;
+    let c = Math.floor(Math.random() * 30) + 70;
+    return `hsl(${a}, ${b}%, ${c}%)`;
+}
+
+document.getElementById("theme-btn").addEventListener("cliick", changeTheme);
+function changeTheme() {
+    let bgColor = getColor();
+    document.body.style.backgroundColor = bgColor;
+    // console.log("thik ache");
+}
