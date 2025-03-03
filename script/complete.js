f339328ad79494ff48e587ea47fbb85bbade856d
Arin Barua <arinbarua1994@gmail.com>
2025-03-03 23:41:07 +0600

    added final solution

--- script/complete.js
+++ script/complete.js
@@ -1,6 +1,4 @@
 
-//   alert("Hello! I am an alert box!");
-
 function getColor() {
     let a = Math.floor(Math.random() * 360);
     let b = Math.floor(Math.random() * 50) + 50;
@@ -18,3 +16,53 @@ function changeTheme() {
 document.getElementById("blog").addEventListener("click", function() {
     window.location.href = "./blog.html";
 });
+
+let taskCount = 6;
+let score = 23;
+
+        function taskBtn(e) {
+            
+            if (taskCount > 0, e==="task 1") {
+                if(taskBtn === true, taskCount--,
+                    score++)
+                
+                // console.log(e);
+                alert("You have submitted the task succesfully!");
+
+                let taskTitle = "Fix Mobile Button Issue"; // Task title
+                let currentTime = new Date("23:14:00");
+                let message = `You have completed the task "${taskTitle}" at ${currentTime}`;
+
+                document.getElementById("taskCount").addEventListener("click", function() {
+                    document.getElementById("score").innerText = taskCount;
+                    document.getElementById("score").innerText = score;
+                    document.getElementById("messageBox").innerText = message;
+                    document.getElementById("messageBox").style.display = "block";
+
+                // Disable the button after completing the task
+                // document.getElementById("completeBtn").disabled = true;
+                // Disable the clicked button
+                button.disabled = true;
+
+                // If last task is completed, show final alert
+                if (taskCount === 0) {
+                    alert("Congratulations! All tasks are completed.");
+                }
+                })
+                
+                // document.getElementById("score").innerText = taskCount;
+                // document.getElementById("score").innerText = score;
+                // document.getElementById("messageBox").innerText = message;
+                // document.getElementById("messageBox").style.display = "block";
+
+                // // Disable the button after completing the task
+                // document.getElementById("completeBtn").disabled = true;
+            } 
+            else {
+                
+            }
+        }
+
+        function clearMessage() {
+            document.getElementById("messageBox").style.display = "none";
+        }
